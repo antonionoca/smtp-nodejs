eb8ec28f0c65fdc862254b572384c332289b1f94
Antonio Noca <antonionoca@gmail.com>
2019-09-26 20:14:21 +0100

    storing messages in memory

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 const client = net.createConnection({port: 8025}, () => {
   // 'connect' listener
   console.log('connected to server');
-  client.write('ping\r\n');
+  client.write('sent ping message');
 });
 client.on('data', (data) => {
   console.log(data.toString());
--- smtpserver.js
+++ smtpserver.js
@@ -5,21 +5,44 @@ const net = require('net');
 */
 class SmtpServer {
   /**
+  * @param {messages} messages stored by the fake smtp server
+  */
+  constructor(messages) {
+    this.messages = messages || [];
+  }
+
+  /**
   * @param {port} port to listen to.
   */
   start(port = 8025) {
     net.createServer((c) => {
       console.log('client connected');
+      c.on('data', (data) => {
+        const msg = data.toString();
+        this.messages.push(msg);
+        console.log(msg);
+      });
       c.on('end', () => {
         console.log('client disconnected');
       });
-      c.write('hello world\r\n');
+      c.write('server response');
       c.pipe(c);
     }).listen(port, () => {
       console.log(`Server bound at port ${port}`);
     });
   }
+  /**
+  * Get captured email messages
+  * @return messages array
+  */
+  getMessages() {
+    return this.messages;
+  }
 }
 
 const server = new SmtpServer();
 server.start();
+
+setInterval(function() {
+  console.log(JSON.stringify(server.getMessages()));
+}, 3000);
